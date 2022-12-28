db4f0645239fb3a4e39c642ceddd6d4eb276afa6
Vlad <59318657+vlad7-wq@users.noreply.github.com>
2022-12-28 19:16:51 +0200

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,2 +1,24 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
+    WebDriver driver;
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://google.com");
+    }
+    @org.junit.Test
+    public void test() {
+        String title = driver.getTitle();
+        boolean testResult = title.contains("Google");
+        Assert.assertTrue(testResult);
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
